4a82c3c4b31ee770d26924a7af356ecfd9ec8a8c
Omni <engomneya.osman@gmail.com>
2019-09-20 22:49:51 +0200

    reformat OnBoarding :adding ViewPager2 with OnBoardingAdapter

--- app/src/main/java/me/inassar/onboardingfreebi/feature/OnBoardingView.kt
+++ app/src/main/java/me/inassar/onboardingfreebi/feature/OnBoardingView.kt
@@ -3,16 +3,17 @@ package me.inassar.onboardingfreebi.feature
 import android.content.Context
 import android.os.Build
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
 import androidx.annotation.RequiresApi
 import androidx.viewpager2.widget.ViewPager2
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator
 import kotlinx.android.synthetic.main.onboarding_view.view.*
 import me.inassar.onboardingfreebi.R
-import me.inassar.onboardingfreebi.feature.onboarding.OnBoardingPagerAdapter
 import me.inassar.onboardingfreebi.feature.onboarding.OnBoardingPage
-import me.inassar.onboardingfreebi.hide
-import me.inassar.onboardingfreebi.show
+import me.inassar.onboardingfreebi.feature.onboarding.OnBoardingPagerAdapter
 import setParallaxTransformation
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
@@ -23,34 +24,35 @@ constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
     private val numberOfPages by lazy { OnBoardingPage.values().size }
 
     init {
-        LayoutInflater.from(context).inflate(R.layout.onboarding_view, this, true)
-        setUpListeners()
-        setUpSlider()
+        val view  = LayoutInflater.from(context).inflate(R.layout.onboarding_view, this, true)
+        setUpSlider(view)
     }
 
-    private fun setUpSlider() {
+    private fun setUpSlider(view :View) {
         with(slider) {
             adapter = OnBoardingPagerAdapter()
             setPageTransformer { page, position ->
                 setParallaxTransformation(page , position)
             }
+
+            addSlideChangeListener()
+
+            val wormDotsIndicator = view.findViewById<WormDotsIndicator>(R.id.page_indicator)
+            wormDotsIndicator.setViewPager2(this)
         }
     }
 
 
-    private fun setUpListeners() {
-//        page_indicator.setViewPager(slider)
+    private fun addSlideChangeListener () {
+
         slider.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
                 super.onPageScrolled(position, positionOffset, positionOffsetPixels)
-                if (position == numberOfPages.minus(1)) {
-                    nextBtn.hide()
-                    skipBtn.hide()
-                    startBtn.show()
-                } else {
-                    nextBtn.show()
-                    skipBtn.show()
-                    startBtn.hide()
+                if (numberOfPages > 1) {
+                    val newProgress = (position + positionOffset) / (numberOfPages - 1)
+
+                    onboardingRoot.progress = newProgress
+                    Log.d("Progress" , onboardingRoot.progress.toString())
                 }
             }
         })
